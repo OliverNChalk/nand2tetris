bf98b50f89969ae1fa88fbc3b32f17ecca131002
Oliver Chalk <oliverchalk245@gmail.com>
2021-08-22 14:57:46 +1000

    HackAssembler: Trim whitespace (including comments)

--- projects/06/hack-assembler/src/main.rs
+++ projects/06/hack-assembler/src/main.rs
@@ -1,3 +1,4 @@
+// use std::collections::HashMap;
 use std::env;
 use std::error::Error;
 use std::fs;
@@ -9,11 +10,32 @@ fn read_lines(file_name: &str) -> Result<Vec<String>, Box<dyn Error>> {
     return Ok(lines);
 }
 
+fn clean_whitespace(mut file: Vec<String>) -> Vec<String> {
+    for i in 0..file.len() {
+        let line = &mut file[i];
+
+        // Remove everything after a comment
+        if let Some(test) = line.find("//") {
+            line.truncate(test);
+        }
+
+        // Remove all whitespace
+        *line = line.trim().to_owned();
+    }
+
+    return file;
+}
+
+// fn translate_line(line: &str) -> String {
+//
+// }
+
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     if let Some(first_arg) = args.get(1) {
         let result = read_lines(&first_arg).unwrap();
+        let result = clean_whitespace(result);
 
         for (i, line) in result.iter().enumerate() {
             println!("{}: {}", i, line);
