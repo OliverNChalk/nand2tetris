8af23ff1b2138d77dd19c9004a5b5dcb84ff1066
Oliver Chalk <oliverchalk245@gmail.com>
2021-08-22 16:38:50 +1000

    HackAssembler: Add A-INST translation, no symbols

--- projects/06/hack-assembler/src/main.rs
+++ projects/06/hack-assembler/src/main.rs
@@ -26,9 +26,42 @@ fn clean_whitespace(mut file: Vec<String>) -> Vec<String> {
     return file;
 }
 
-// fn translate_line(line: &str) -> String {
-//
-// }
+fn translate_line(line: &str) -> String {
+    let first_char = line.chars().nth(0);
+
+    return match first_char {
+        None => "EMPTY LINE".to_owned(),
+        Some('(') => "SYMBOL".to_owned(),
+        Some('@') => translate_a(line).unwrap_or("".to_owned()),
+        _ => translate_c(line),
+    };
+}
+
+fn translate_a(line: &str) -> Option<String> {
+    let symbol = &line[1..];
+
+    let result = symbol
+        .parse::<u16>()
+        .and_then(|x| Ok(format!("{:016b}", x)))
+        .expect(&format!("Failed to parse to A-INST {}", symbol));
+
+    let lead_bit = result.chars().nth(0).unwrap();
+    assert_eq!(lead_bit, '0', "{} lead bit was not zero", lead_bit);
+
+    return Some(result);
+}
+
+fn translate_c(line: &str) -> String {
+    if let Some((dest, _)) = line.split_once('=') {
+        return format!("DEST: {}", dest);
+    }
+
+    if let Some((_, jump)) = line.split_once(';') {
+        return format!("JUMP: {}", jump);
+    } else {
+        return format!("INVALID C-INST");
+    }
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -39,6 +72,7 @@ fn main() {
 
         for (i, line) in result.iter().enumerate() {
             println!("{}: {}", i, line);
+            println!("{}", translate_line(line));
         }
     }
 }
