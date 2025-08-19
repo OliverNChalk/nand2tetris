f1cfac5a3487610ef5c15016f5f7b3dde4891bfe
OliverNChalk <11343499+OliverNChalk@users.noreply.github.com>
2025-08-19 15:48:51 -0500

    init jack crate

---
+++ crates/jack/src/args.rs
@@ -0,0 +1,6 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+#[command(propagate_version = true)]
+pub(crate) struct Args {}
---
+++ crates/jack/src/main.rs
@@ -0,0 +1,10 @@
+mod args;
+
+fn main() {
+    use clap::Parser;
+
+    // Parse command line args.
+    let args = args::Args::parse();
+
+    println!("COMING SOON");
+}
